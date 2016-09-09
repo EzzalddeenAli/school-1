02ee686ab85a40ab629b978daa88dc4bb5854b65
Abdul Manan <abdulmanan7@hotmail.com>
2016-09-10 00:53:57 +0500

    header link changed

--- upload/app/views/Layout_new.blade.php
+++ upload/app/views/Layout_new.blade.php
@@ -135,15 +135,11 @@
             <img src="{{URL::to('/dashboard/profileImage/'.$users['id'])}}" class="img-responsive img-circle">
           </a>
           <ul class="dropdown-menu userinfo arrow">
-            <li><a href="#/"><i class="ti ti-user"></i><span>Profile</span><span class="badge badge-info pull-right">80%</span></a></li>
-            <li><a href="#/"><i class="ti ti-panel"></i><span>Account</span></a></li>
-            <li><a href="#/"><i class="ti ti-settings"></i><span>Settings</span></a></li>
+            <li><a href="#/"><i class="ti ti-user"></i><span>Profile</span></a></li>
             <li class="divider"></li>
-            <li><a href="#/"><i class="ti ti-stats-up"></i><span>Earnings</span></a></li>
-            <li><a href="#/"><i class="ti ti-view-list-alt"></i><span>Statement</span></a></li>
             <li><a href="#/"><i class="ti ti-money"></i><span>Withdrawals</span></a></li>
             <li class="divider"></li>
-            <li><a href="#/"><i class="ti ti-shift-right"></i><span>Sign Out</span></a></li>
+            <li><a href="<?php echo URL::to('/logout');?>"><i class="ti ti-shift-right"></i><span><?php echo $panelInit->language['logout'];?></span></a></li>
           </ul>
         </li>
       </ul>
